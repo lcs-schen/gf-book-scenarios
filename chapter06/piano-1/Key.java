c8a654ed07925fd0b7a7d8423415daa8e52e74e8
sachen@lcs.on.ca <sachen@lcs.on.ca>
2019-04-26 13:17:01 -0400

    touch edge create leaf

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -4,21 +4,26 @@ public class Key extends Actor
 {
     /**
      * Instance variables
-     * These can be used anywhere i the class below
+     * These can be used anywhere in the class below.
      */
-    //Will keep track of thether a key was just pressed
+    // Will keep track of whether a key was just pressed
     private boolean keyAlreadyDown;
-
-    
+    // Will contain the key and sound file that this instance of the class will play
+    private String key;
+    private String sound;
     
     /**
      * Create a new key.
+     * Constuctor – it runs once when a Key object is created.
      */
-    public Key()
+    public Key(String keyName, String soundFile)
     {
-        //No key has been pressed
+        // No key has been pressed
         keyAlreadyDown = false;
-
+        
+        // Set the instance variables using the parameters
+        key = keyName;
+        sound = soundFile;
     }
 
     /**
@@ -26,27 +31,27 @@ public class Key extends Actor
      */
     public void act()
     {
-        //Animate the piano key being presses
-        //Condition 1 - is thr "g" key being pressed
-        //Condition 2 - was the "g" key already down
-        if(Greenfoot.isKeyDown("g") && keyAlreadyDown == false )
+        // Animate the piano key being pressed
+        // Condition 1 – is the "g" key being pressed
+        // Condition 2 - the key was NOT already down
+        if ( Greenfoot.isKeyDown(key) && !keyAlreadyDown)
         {
-            //Key is down
+            // Key is down
             setImage("white-key-down.png");
             play();
             keyAlreadyDown = true;
         }
-        
-        //Stop showing the key being pressed
-        //Condition 1 - The key was down the last time act() tired
-        //Condition 2 - The "g" has been released no longer being presses
-        if (keyAlreadyDown == true && Greenfoot.isKeyDown("g") == false)
+
+        // Stop showing the key being pressed
+        // Condition 1 - The key was down the last time act() fired
+        // Condition 2 - The "g" has been released (no longer being pressed)
+        if ( keyAlreadyDown == true && !Greenfoot.isKeyDown(key))
         {
-            //Key is up
+            // Key is up
             setImage("white-key.png");
             keyAlreadyDown = false;
         }
-
+        
     }
 
     /**
@@ -54,7 +59,23 @@ public class Key extends Actor
      */
     public void play()
     {
-        Greenfoot.playSound("3a.wav");
+        Greenfoot.playSound(sound + ".wav");
     }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -9,15 +9,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Piano extends World
 {
     /**
-     * Instance variable
-     * (we can use these variable anywhere in the class)
+     * Instance variables
+     * (we can use these variables anywhere in the class)
      */
     int frames;
-    
-    //Create the array(list of names)
-    String[] peopleInClass = {"Julio","Liam","Osman","Simon","Eric","Phillip",
-            "Joven","Joe","Gordan","Nick","Jieun","Selena","Jason","Sam","Jeff"
+
+    // Create the array (list of names)
+    String[] peopleInClass = {"Julio", "Liam", "Dima", "Osman", "Nick", "Simon", "Eric", "Phillip", 
+            "Joven", "Joe", "Gordon", "Jieun", "Selena", "Jason", "Sam", "Jeff"
         };
+
+    //Create two arrays
+    //First array tracks the keyboard keys that we will use for white piano keys
+    String[] whiteKeys = {"a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "," , "\\"};
+    //Sound array tracks the sounds files for each note
+    String[] whiteNotes = {"3c","3d", "3e", "3f", "3g", "3a", "3b", "4c", "4d", "4e", "4f", "4g"};
+    
+    String[] blackKeys = {"w", "e", "", "+","y", "u", "","o", "p", "","]", ""};
+    
+    String[] blackNotes = {"w", "e", "", "+","y", "u", "","o", "p", "","]", ""};
 
     /**
      * Make the piano.
@@ -26,32 +36,43 @@ public class Piano extends World
     {
         super(800, 340, 1);
 
-        //Initialize instance variable
+        // Initialize instance variables
         frames = 0;
-
     }
 
     /**
      * act
-     * (gets called repeatly to create animation)
+     * (gets called repeatedly to create animation)
      */
     public void act()
     {
-        //Say "hello" to a different person each second
-        if ((frames % 60 == 0) && (frames / 60 < 16) )
+        // This block of code runs once per second until the end of the
+        // array
+        int position = frames / 60;
+        //     every FULL SECOND     when the position is less than count of items in the array
+        if ( (frames % 60 == 0) && (position < whiteKeys.length) )
         {
-            //Only shoe a mesage when we are in the bounds of the array
-            //showText("array index is:" + frames / 60, 400, 250);
+            // Only show a message when we are in the bounds of the array
+            showText("Array index is: " + position, 400, 250);
 
-            //Only say hello when we are in the bounds of the array
-            //Say hello to everyone in the class!
-            //showText("Hello " + peopleInClass[frames / 60], 400, 170);
+            //Create and object to add to the world
+            Key aKey = new Key(whiteKeys[position], whiteNotes[position]);
+            //Now actually add the obbkect to the world
+            //        OBJEXT TOO ADD    HORIZONTAL POSITION   VERTICAL POSITION
+            addObject(aKey, 54 + position * 63, 140);
+
+            // Only say hello when we are in the bounds of the array
+            // Say hello to everyone in the class! 
+            // showText("Hello " + peopleInClass[frames / 60], 400, 170);
+
         }
 
-        //tack frames
+        // Track frames
         frames += 1;
 
-        //show the frames
-        //showText("" + frames, 100, 100);
+        // Show the frames
+        // showText("" + frames, 100, 100);
     }
+
 }
+
